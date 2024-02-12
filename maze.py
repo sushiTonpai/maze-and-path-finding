e8d738a03c48e7540dc33929258a148d5a5a0e7a
tonpai <paigood999@gmail.com>
2024-02-12 16:26:24 +0700

    add requirements.txt and maze walk

--- maze.py
+++ maze.py
@@ -3,7 +3,6 @@ import sys
 import random
 from dataclasses import dataclass
 
-
 # Define constants
 WIDTH, HEIGHT = 600, 600
 CELL_SIZE = 40
@@ -14,58 +13,46 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (34, 139, 34)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+RED = (129, 0, 0)
 
-@dataclass 
-class cell:
+
+@dataclass
+class Cell:
     x: int
     y: int
     visited: bool = False
 
     def draw(self):
-        x , y = self.x * CELL_SIZE, self.y * CELL_SIZE
-        if self.visited == True: 
-            pygame.draw.rect(screen, WHITE, (x,y,CELL_SIZE,CELL_SIZE))
+        x, y = self.x * CELL_SIZE, self.y * CELL_SIZE
+        if self.visited:
+            pygame.draw.rect(screen, WHITE, (x, y, CELL_SIZE, CELL_SIZE))
 
         # TOP WALL
-        pygame.draw.line(screen, GREEN, (x,y) , (x + CELL_SIZE , y), 3)
+        pygame.draw.line(screen, GREEN, (x, y), (x + CELL_SIZE, y), 3)
         # BOTTOM WALL
-        pygame.draw.line(screen, GREEN, (x, y + CELL_SIZE) , (x + CELL_SIZE, y + CELL_SIZE), 3)
+        pygame.draw.line(screen, GREEN, (x, y + CELL_SIZE), (x + CELL_SIZE, y + CELL_SIZE), 3)
         # LEFT WALL 
-        pygame.draw.line(screen, GREEN, (x,y) , (x, y + CELL_SIZE) , 3)
+        pygame.draw.line(screen, GREEN, (x, y), (x, y + CELL_SIZE), 3)
         # RIGHT WALL
-        pygame.draw.line(screen, GREEN, (x + CELL_SIZE, y), (x + CELL_SIZE , y + CELL_SIZE), 3)
-
+        pygame.draw.line(screen, GREEN, (x + CELL_SIZE, y), (x + CELL_SIZE, y + CELL_SIZE), 3)
 
     # find allow neighbour cells
-    def neighbour(self):
-        x , y = self.x, self.y
+    def walk_to_neighbour(self, maze):
+        x, y = self.x, self.y
         neighbour = []
-        #adjacent cell
-        for i,j in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        # adjacent cell
+        for i, j in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
             new_cell = (x + i, y + j)
             if 0 <= new_cell[0] < ROWS and 0 <= new_cell[1] < COLS:
-                if cell(new_cell[0],new_cell[1]).visited == False:
-                    print('visiting = ', new_cell, cell(new_cell[0],new_cell[1]))
+                if not maze[new_cell[0]][new_cell[1]].visited:
+                    print('visiting = ', new_cell, maze[new_cell[0]][new_cell[1]])
                     neighbour.append(new_cell)
         if neighbour:
             walk_to_cell = random.choice(neighbour)
-            return cell(walk_to_cell[0],walk_to_cell[1])
+            return maze[walk_to_cell[0]][walk_to_cell[1]]
         else:
             return False
-
-    
-    # def walk(neighbors):
-    #     if neighbors:
-    #         walk_to_cell = random.choice(neighbors)
-    #         return cell(walk_to_cell[0],walk_to_cell[1])
-    #     else:
-    #         return False
-
-
-
-
-# print(cell.walk(cell.neighbour(cell(0,0))))
-
 
 
 # Initialize Pygame
@@ -74,11 +61,10 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Maze")
 clock = pygame.time.Clock()
 
-        
-[cell(row, col) for row in range(ROWS) for col in range(COLS)]
-current_cell = cell(0,0)
+# maze = [Cell(row, col) for row in range(ROWS) for col in range(COLS)]
+maze = [[Cell(row, col) for col in range(COLS)] for row in range(ROWS)]
+current_cell = maze[0][0]
 stack = []
-
 
 screen.fill(BLACK)
 
@@ -89,16 +75,16 @@ while True:
             pygame.quit()
             sys.exit()
 
-    [cell(row, col).draw() for row in range(ROWS) for col in range(COLS)]
+    # [Cell(row, col).draw() for row in range(ROWS) for col in range(COLS)]
 
     current_cell.visited = True
     current_cell.draw()
     print(current_cell)
 
-    next_cell  = current_cell.neighbour()
+    next_cell = current_cell.walk_to_neighbour(maze=maze)
     if next_cell:
         current_cell = next_cell
         current_cell.visited = True
-        
+
     pygame.display.flip()
     clock.tick(5)
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 import random
+
 adj = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 print(random.choice(adj))
+
+ROWS = 2
+COLS = 3
+array_2d = [[i for i in range(0, COLS)] for j in range(0, ROWS)]
+print(array_2d)
+print(array_2d[0][1])
