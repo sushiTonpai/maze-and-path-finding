41237ae248c8955017e3a402348cb74bfbd99a3b
tonpai <paigood999@gmail.com>
2024-02-14 16:41:58 +0700

    code comment

--- cell.py
+++ cell.py
@@ -7,6 +7,19 @@ from constants import CELL_SIZE, WHITE, YELLOW, GREEN, ROWS, COLS
 import random
 import pygame
 
+'''
+the Cell class have draw and walk_to_neighbor functions:
+draw is to draw block with green walls
+    cell block background yellow = searching path(in stack)
+    cell block background white = done searching path(not in stack)
+    
+walk_to_neighbor is function that return walkable cell with path taken
+    eg. walk south from Cell(0, 0) ->
+            walk_to_cell = (0,2), walk_path = (0,1)
+        walk west from Cell (4,5) ->
+            walk_to_cell = (2,5), walk_path = (3,5)
+'''
+
 
 @dataclass
 class Cell:
@@ -16,9 +29,10 @@ class Cell:
     in_stack: bool = False
 
     def draw(self, screen):
+        # pixel position of cell = x,y
         x, y = self.x * CELL_SIZE, self.y * CELL_SIZE
         if self.visited and not self.in_stack:
-            pygame.draw.rect(screen, WHITE, (x, y, CELL_SIZE, CELL_SIZE))
+            pygame.draw.rect(screen,WHITE, (x, y, CELL_SIZE, CELL_SIZE))
         if self.in_stack and self.visited:
             pygame.draw.rect(screen, YELLOW, (x, y, CELL_SIZE, CELL_SIZE))
 
@@ -32,18 +46,28 @@ class Cell:
         pygame.draw.line(screen, GREEN, (x + CELL_SIZE, y), (x + CELL_SIZE, y + CELL_SIZE), 3)
 
     # find allow neighbour cells
-    def walk_to_neighbour(self, maze: Maze) -> Union[tuple[Cell, Cell], tuple[bool,bool]]:
+    def walk_to_neighbour(self, maze: Maze) -> Union[tuple[Cell, Cell], tuple[bool, bool]]:
+        """
+        neighbour is dict contain key of walkable call with value of path taken then choose randomly in neighbour
+        dict
+        return walk_to_cell = Cell or False if nothing in neighbour dict , walk_path = Cell or False if nothing
+        in neighbour dict
+        :param maze:
+        """
         x, y = self.x, self.y
-        # list of walkable cells
+        # dict of walkable cells
         neighbours = {}
         # adjacent cell
         for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            # new path and cell in direction of dx and dy
             new_path = (x + dx, y + dy)
             new_cell = (x + dx * 2, y + dy * 2)
+            # out of bound check and Cell is visited
             if 0 <= new_cell[0] < ROWS and 0 <= new_cell[1] < COLS:
                 if not maze[new_cell[0]][new_cell[1]].visited:
-                    print('visiting = ', new_cell, maze[new_cell[0]][new_cell[1]])
+                    # print('visiting = ', new_cell, maze[new_cell[0]][new_cell[1]])
                     neighbours.update({new_cell: new_path})
+        # check item in neighbours if not return False
         if neighbours:
             walk_to_cell, walk_path = random.choice(list(neighbours.items()))
             return maze[walk_to_cell[0]][walk_to_cell[1]], maze[walk_path[0]][walk_path[1]]
@@ -51,4 +75,5 @@ class Cell:
             return False, False
 
 
+# Type annotation
 Maze = list[list[Cell]]
--- maze.py
+++ maze.py
@@ -10,7 +10,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Maze")
 clock = pygame.time.Clock()
 
-# maze = [Cell(row, col) for row in range(ROWS) for col in range(COLS)]
+# initial pygame setup
 maze: list[list[Cell]] = [[Cell(row, col) for col in range(COLS)] for row in range(ROWS)]
 current_cell = maze[0][0]
 stack = [current_cell]
@@ -21,28 +21,32 @@ screen.fill(BLACK)
 
 # Main game loop
 while True:
+    # exit game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # [Cell(row, col).draw() for row in range(ROWS) for col in range(COLS)]
     current_cell.draw(screen=screen)
-    print(current_cell)
+    # print(current_cell)
 
-    next_cell, next_path = current_cell.walk_to_neighbour(maze=maze)
-
+    next_cell, path = current_cell.walk_to_neighbour(maze=maze)
+    """
+    if there is next cell we put nex cell and path taken in stack, 
+    update in_stack and visited of the next cell and path taken = True then draw cells
+    if there is not next cell we roll back the visited cells in stack. and draw cells
+    """
     if next_cell:
         current_cell = next_cell
-        next_path.in_stack, next_path.visited = True, True
+        path.in_stack, path.visited = True, True
         current_cell.in_stack, current_cell.visited = True, True
-        # stack.append(next_path)
+        # stack.append(path)
         stack.append(current_cell)
-        next_path.draw(screen=screen)
+        path.draw(screen=screen)
         current_cell.draw(screen=screen)
     elif not next_cell and stack:
         current_cell = stack.pop()
-        # next_path = stack.pop()
+        # path = stack.pop()
         current_cell.in_stack = False
         current_cell.draw(screen=screen)
 
