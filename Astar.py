1d445fa520879158a38d217abf37f7279dd1d5e4
pai <paigood999@gmail.com>
2024-02-29 17:32:28 +0700

    Astar class

--- Astar.py
+++ Astar.py
@@ -14,11 +14,13 @@ class Node:
     H(n) distance calculate by manhattan distance -> abs(current.x - goal.x) + abs(current.y - goal.y)
 
     """
+
+    current_node: Cell
     g_cost: int
     h_cost: int
     parents: Cell
-    start: Cell
-    goal: Cell
+    start_node: Cell
+    goal_node: Cell
 
     def f_cost(self):
         return self.g_cost + self.h_cost
@@ -30,17 +32,17 @@ class Node:
     def cost_to_node(self, neighbour: Node) -> int:  # calculate G(n) cost
         return self.g_cost + neighbour.get_distance()
 
+    def walkable_path(self, maze) -> list[Cell] or bool:
+        neighbours = []
+        # 4 direction [(west)(east)(north)(south)]
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            cell_neighbour = self.current_node.x + dx, self.current_node.y + dy
+            if 0 <= cell_neighbour[0] < len(maze[0]) and 0 <= cell_neighbour[1] < len(maze):  # out of bound check
+                if self.current_node.visited:  # not a wall check
+                    neighbours.append(cell_neighbour)
+        return neighbours if neighbours else False
 
-def walkable_path(cell, maze) -> list[Cell] or bool:
-    neighbours = []
-    # 4 direction [(west)(east)(north)(south)]
-    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        cell_neighbour = cell.x + dx, cell.y + dy
-        if 0 <= cell_neighbour[0] < len(maze[0]) and 0 <= cell_neighbour[1] < len(maze):  # out of bound check
-            if cell.visited:  # not a wall check
-                neighbours.append(cell_neighbour)
-    return neighbours if neighbours else False
+    def estimate_cost(self) -> int:  # calculate heuristic cost
+        return abs(self.goal_node.x - self.current_node.x) + abs(self.goal_node.y - self.current_node.y)
 
 
-def estimate_cost(goal: Cell, current: Cell) -> int:  # calculate heuristic cost
-    return abs(goal.x - current.x) + abs(goal.y - current.x)
